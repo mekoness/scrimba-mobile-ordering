27b634a3de4b71aac3f60e60dc9720817f706add
mekoness <mekoness@gmail.com>
2022-12-15 01:08:56 -0800

    Ver 1

---
+++ data.js
@@ -0,0 +1,26 @@
+export const menuData = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 14,
+        emoji: "🍕",
+        quantity: 0,
+        id: 0
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        quantity: 0,
+        id: 1
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        quantity: 0,
+        id: 2
+    }
+]
---
+++ index.js
@@ -0,0 +1,131 @@
+import {menuData} from "./data.js";
+let myOrder = []
+
+document.addEventListener("click", e => {
+    if (e.target.dataset.id) {
+        handleOrderClick(e.target.dataset.id)
+    } else if (e.target.dataset.remove) {
+        handleRemoveClick(e.target.dataset.remove)
+    } else if (e.target.id === "order-btn") {
+        handleOrderBtnClick()
+    } else if (e.target.id === "pay-btn") {
+        handlePayBtnClick()
+    };
+});
+
+function render() {
+    document.querySelector("#menu").innerHTML = getMenuHtml();
+};
+
+render();
+
+function getMenuHtml() {
+    let menuHtml = ``;
+
+    menuData.forEach(menuItem => {
+        menuHtml += `
+            <div class="menu-container">
+                <div class="menu-item">
+                    <div class="menu-emoji">
+                        <p class="item-emoji">${menuItem.emoji}</p>
+                    </div>
+                    <div class="menu-details">
+                        <p class="item-name">${menuItem.name}</p>
+                        <p class="item-ingredients">${menuItem.ingredients}</p>
+                        <p class="item-price">$${menuItem.price}</p>
+                    </div>
+                </div>
+                <div>
+                    <button class="menu-btn" data-id="${menuItem.id}">+</button>
+                </div>
+            </div>
+        `
+    });
+    return menuHtml;
+};
+
+function renderOrders() {
+    document.querySelector("#order-feed").innerHTML = getOrderHtml();
+}
+
+function getOrderHtml() {
+    let orderHtml = ``;
+
+    myOrder.forEach(orderItem => {
+        orderHtml += `
+            <div class="order-container">
+                <div class="order-item" data-item="${orderItem.id}">
+                    <p class="order-name">${orderItem.name}</p>
+                    <p class="order-quantity" id="order-area-${orderItem.id}">x ${orderItem.quantity}</p>
+                    <button class="remove-btn" id="remove-id-${orderItem.id}" data-remove="${orderItem.id}">remove</button>
+                </div>
+                <div>
+                    <p class="item-price" id="item-price-${orderItem.id}">$${orderItem.price * orderItem.quantity}</p>
+                </div>
+            </div>
+        `
+    });
+    return orderHtml;
+};
+
+function renderPrice() {
+    const priceArea = document.querySelector("#price-total")
+    let priceTotal = 0
+
+    myOrder.forEach(orderItem => {
+        priceTotal += orderItem.price * orderItem.quantity;
+    });
+    priceArea.textContent = `$${priceTotal}`;
+};
+
+function handleOrderClick(itemId) {
+    const menuObj = menuData.filter(menuItem => parseInt(menuItem.id) === parseInt(itemId))[0];
+
+    if (myOrder.includes(menuObj)) {
+        menuObj.quantity ++;
+    } else {
+        menuObj.quantity ++;
+        myOrder.push(menuObj);
+        document.querySelector("#order").classList.remove("hidden");
+    };
+    renderPrice();
+    renderOrders();
+};
+
+function handleRemoveClick(itemId) {
+    const menuObj = myOrder.filter(menuItem => parseInt(menuItem.id) === parseInt(itemId))[0];
+
+    if (menuObj.quantity !== 0) {
+        menuObj.quantity --;
+    } else if (menuObj.quantity === 0) {
+        myOrder.filter((orderItem, index) => {
+            if (orderItem.id === itemId) {
+                myOrder.splice(index, 1)
+            };
+        });
+    };
+    renderPrice();
+    renderOrders();
+};
+
+function handleOrderBtnClick() {
+    document.querySelector("#modal").classList.remove("hidden");
+};
+
+function handlePayBtnClick() {
+    const modalInputs = document.querySelectorAll(".modal-input")
+    let name = document.querySelector("#name").value
+
+    modalInputs.forEach(input => {
+        if (input.value.length === 0) {
+            alert(`${input.name} required.`)
+        } else {
+            document.querySelector("#modal").classList.add("hidden");
+            document.querySelector("#order").innerHTML = `
+                <div class="confirm">
+                    <p>Thanks, ${name}! Your order is on its way!</p>
+                </div>
+            `
+        };
+    });
+};
